714cb3eb000277dfcb6e8736290b4dc44189f3ba
bazaltt <fedotovadhome@gmail.com>
2024-05-06 19:55:19 +0300

    Вернул обработик ошибок

--- project/errors/handlers.py
+++ project/errors/handlers.py
@@ -3,12 +3,12 @@ from project import db
 from project.errors import bp
 
 
-# @bp.app_errorhandler(404)
-# def not_found_error(error):
-#     return render_template('errors/404.html'), 404
-#
-#
-# @bp.app_errorhandler(500)
-# def internal_error(error):
-#     db.session.rollback()
-#     return render_template('errors/500.html'), 500
+@bp.app_errorhandler(404)
+def not_found_error(error):
+    return render_template('errors/404.html'), 404
+
+
+@bp.app_errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('errors/500.html'), 500

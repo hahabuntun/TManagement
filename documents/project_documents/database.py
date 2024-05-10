5b809fa888fabe6a224856ccf7685924a116fe1e
Пользователь <hahabuntun@gmail.com>
2024-05-10 23:39:51 +0300

    fixed

--- documents/project_documents/database.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
-
-engine = create_engine("postgresql://postgres:7NoTaizai@localhost:5432/pizza_delivery", echo=True)
-
-Base = declarative_base()
-
-Session = sessionmaker()
